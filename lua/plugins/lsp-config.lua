06ba0c1c823ff645a8e4229e6e66f2419dee6dcd
PravinC-Dev <pravinhchauhan18@gmail.com>
2025-10-31 10:41:57 -0600

    Update lsp-config.lua

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,5 +1,5 @@
 return {
-  {  
+  {
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup({
@@ -11,7 +11,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "tsserver", "pyright" }
+        ensure_installed = { "vtsls", "pyright", "lua_ls" }
       })
     end
   },
@@ -19,33 +19,37 @@ return {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
-
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
       local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({
+      lspconfig.vtsls.setup({
         capabilities = capabilities,
-        filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact", "javascript.jsx" },
-        cmd = { "/home/pravin/.nvm/versions/node/v20.12.2/bin/typescript-language-server", "--stdio" },
-        init_options = {
-          preferences = {
-            disableSuggestions = true,
-          }
-        }
+        filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact", "typescript.tsx", "javascript.jsx" },
+        cmd = { "/usr/local/bin/vtsls", "--stdio" },
       })
       lspconfig.html.setup({
         capabilities = capabilities,
-        cmd = {"/home/pravin/.nvm/versions/node/v20.12.2/bin/vscode-html-language-server", "--stdio"},
+        cmd = { "/home/pravin/.nvm/versions/node/v20.12.2/bin/vscode-html-language-server", "--stdio" },
       })
 
       lspconfig.cssls.setup({
         capabilities = capabilities,
-        cmd = {"/home/pravin/.nvm/versions/node/v20.12.2/bin/vscode-css-language-server", "--stdio"},
+        cmd = { "vscode-css-language-server", "--stdio" },
+        filetypes = { "css", "scss", "less" }
       })
 
       lspconfig.pyright.setup({
         capabilities = capabilities,
-        
+
+      })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+        cmd = { "/opt/lua-language-server/bin/lua-language-server", "--stdio" }
+      })
+
+      lspconfig.rust_analyzer.setup({
+        capabilities = capabilities,
+        cmd = { "/home/Blegh/.cargo/bin/rust-analyzer" }
       })
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
