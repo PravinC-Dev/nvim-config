106c30f11af64cfa3abefbba106c401c3f04d63d
Mahroom <mahroom6489@gmail.com>
2026-08-14 18:28:26 +0530

    new lsps

--- plugin/lsp-config.lua
+++ plugin/lsp-config.lua
@@ -1,28 +1,28 @@
-vim.pack.add({ 
-    'https://github.com/mason-org/mason-lspconfig.nvim', 
-    'https://github.com/mason-org/mason.nvim', 
+vim.pack.add({
+	"https://github.com/mason-org/mason-lspconfig.nvim",
+	"https://github.com/mason-org/mason.nvim",
 })
 
 require("mason").setup({})
 
 require("mason-lspconfig").setup({
-    ensure_installed = { "lua_ls", "clangd", "pyright", "ts_ls", "html", "cssls" },
-    automatic_enable = true,
+	ensure_installed = { "lua_ls", "clangd", "pyright", "ts_ls", "html", "cssls", "gopls" },
+	automatic_enable = true,
 })
 
 -- vim.lsp.config("*", { capabilities = vim.lsp.protocol.make_client_capabilities() })
 vim.lsp.config("lua_ls", {
-    settings = {
-        Lua = {
-            diagnostics = { globals = { "vim", "require" } },
-            workspace = { checkThirdParty = false },
-            telemetry = { enable = false },
-        },
-    },
+	settings = {
+		Lua = {
+			diagnostics = { globals = { "vim", "require" } },
+			workspace = { checkThirdParty = false },
+			telemetry = { enable = false },
+		},
+	},
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(event)
-        vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, { buffer = event.buf, desc = "Format" })
-    end,
+	callback = function(event)
+		vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, { buffer = event.buf, desc = "Format" })
+	end,
 })
